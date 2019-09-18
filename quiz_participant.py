7d8646be57752cca32207e064754ba80de8a6386
m-bowley <=>
2019-09-18 16:08:59 +0100

    Editing participant

--- quiz_participant.py
+++ quiz_participant.py
@@ -4,12 +4,4 @@ from time import sleep
 quiz_server = nwz.discover("Quiz")
 connection = nwz.advertise(nwz.address())
 reply = nwz.send_message_to(quiz_server, nwz.address())
-while True:
-    reply = nwz.wait_for_message_from(quiz_server, wait_for_s=0)
-    if reply is not None:
-        print(reply)
-    else:
-        reply = nwz.wait_for_message_from(nwz.address(), wait_for_s=0)
-        if reply is not None:
-            print(reply)
-        sleep(1)
+print(reply)

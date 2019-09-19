b923d0a291d6d6b696138b64a4408bcf5c5d4b95
m-bowley <=>
2019-09-19 12:08:24 +0100

    Edits to participant for continous questioning

--- quiz_participant.py
+++ quiz_participant.py
@@ -18,13 +18,24 @@ while not quiz_starting:
             if reply == "Quiz Starting":
                 quiz_starting = True
         sleep(1)
-
+print("Quiz loop starting")
 quiz_over = False
 while not quiz_over:
     reply = nwz.wait_for_message_from(connection, wait_for_s=0, autoreply=True)
     if reply is not None:
-        print(reply)
+        #print(reply)
         if reply == "Next Question":
-            question = nwz.wait_for_message_from(connection)
+            question = nwz.wait_for_message_from(connection, autoreply=True)
             print(question)
             reply = None
+            answers = nwz.wait_for_message_from(connection)
+            for i in range(len(answers)):
+                print(str(i) + ". " + answers[i])
+            print('Enter the number of your answer')
+            my_answer = int(input(">"))
+            nwz.send_reply_to(connection, my_answer)
+            correct = nwz.wait_for_message_from(connection)
+            print(correct)
+            nwz.send_reply_to(connection, None)
+    else:
+        sleep(1)
